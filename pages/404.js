18d88624b862c6744c600326e0ab7886e3a0daea
Derek Hsieh <devderekhsieh@gmail.com>
2022-05-01 17:46:13 -0700

    cusotm 404 page

---
+++ pages/404.js
@@ -0,0 +1,36 @@
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import Router from "next/router";
+
+export default function Custom404() {
+  return (
+    <Box textAlign="center" py={10} px={6}>
+      <Heading
+        display="inline-block"
+        as="h2"
+        size="2xl"
+        bgGradient="linear(to-r, blue.400, blue.600)"
+        backgroundClip="text"
+      >
+        404
+      </Heading>
+      <Text fontSize="18px" mt={3} mb={2}>
+        Page Not Found
+      </Text>
+      <Text color={"gray.500"} mb={6}>
+        The page you're looking for does not seem to exist
+      </Text>
+
+      <Button
+        colorScheme="blue"
+        bgGradient="linear(to-r, blue.400, blue.500, blue.600)"
+        color="white"
+        variant="solid"
+        onClick={function () {
+          Router.back().back();
+        }}
+      >
+        Go to Previous Page
+      </Button>
+    </Box>
+  );
+}
