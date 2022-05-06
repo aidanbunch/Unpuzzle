abc72936ee6b282219648fd72ef84da1ea6b85bd
derekhsiehdev <devderekhsieh@gmail.com>
2022-05-06 11:58:13 -0700

    finish login and signup

---
+++ pages/login.js
@@ -0,0 +1,101 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    InputGroup,
+    HStack,
+    InputRightElement,
+    Stack,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Link,
+  } from '@chakra-ui/react';
+  import { useState } from 'react';
+  import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';  
+  import Navbar from "../components/Navbar"
+  import Footer from "../components/Footer"
+
+  export default function SignupCard() {
+    const [showPassword, setShowPassword] = useState(false);
+  
+    return (
+        <Box
+        minHeight={{
+          base: ["-webkit-fill-available", "fill-available", "-moz-available"],
+          lg: "100vh",
+        }}
+        display="flex"
+        flexDirection="column"
+      >
+
+        <Navbar/>
+      <Flex
+        minH={'100vh'}
+        align={'top'}
+        justify={'center'}
+        // bg={useColorModeValue('gray.50', 'gray.800')}
+        >
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Stack align={'center'}>
+            <Heading fontSize={'4xl'} textAlign={'center'}>
+                Log in
+            </Heading>
+            <Text fontSize={'lg'} color={useColorModeValue('gray.600', 'gray.200')}>
+              to enjoy all of our cool features ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+           
+              <FormControl id="email" isRequired>
+                <FormLabel>Email address</FormLabel>
+                <Input type="email" />
+              </FormControl>
+              <FormControl id="password" isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input type={showPassword ? 'text' : 'password'} />
+                  <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Stack spacing={10} pt={2}>
+                <Button
+                  loadingText="Submitting"
+                  size="lg"
+                  bg={'blue.400'}
+                  color={'white'}
+                  _hover={{
+                    bg: 'blue.500',
+                  }}>
+                    Log in
+                </Button>
+              </Stack>
+              <Stack pt={6}>
+                <Text align={'center'}>
+                  Don't have an account? <Link href={"/signup"} color={'blue.400'}>Sign Up</Link>
+                </Text>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
+      <Footer/>
+      </Box>
+    );
+  }
