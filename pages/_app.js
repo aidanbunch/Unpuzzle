29adb3b18cad24614b09c35a08fd285bb5722e21
aidanbunch <aidanb08@icloud.com>
2022-05-13 23:05:02 -0700

    added code for google analytics and adsense

--- pages/_app.js
+++ pages/_app.js
@@ -3,19 +3,60 @@ import UserProvider from "../context/user";
 import theme from "../theme.js";
 import Head from "next/head";
 import Navbar from "../components/Navbar";
+import Script from 'next/script'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
+import { useEffect } from "react"
 
 function MyApp({ Component, pageProps }) {
+  const G_AD_CLIENT = process.env.G_AD_CLIENT
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Script
+        id="Adsense-id"
+        async={true}
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        data-ad-client={`${G_AD_CLIENT}`}
+        onError={ (e) => { console.error('Auto-ad script failed to load', e) }}
+      />
+
       <Head>
         <title>Unpuzzle</title>
         <link rel="icon" type="image/png" href="/logo.png" />
         <link rel="manifest" href="/manifest.json" />
-        {/* <script id="Adsense-id" data-ad-client="ca-pub-4915743237608302"
-            async strategy="afterInteractive"
-            onError={(e) => { console.error("Script failed to load", e) }}
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          /> */}
       </Head>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
