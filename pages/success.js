61a9bf4ee0742eec65583534f9e9f48ee4766167
derekhsiehdev <devderekhsieh@gmail.com>
2022-05-27 15:10:03 -0700

    add confetti, still needs to fix sizing

---
+++ pages/success.js
@@ -0,0 +1,74 @@
+import { useState, useEffect } from 'react';
+import useWindowSize from '../utils/useWindowDimensions';
+import Confetti from 'react-confetti'
+import { Stack, VStack, Heading, Button, Flex, useColorModeValue, Box, Center, Text } from "@chakra-ui/react"
+import { CheckCircleIcon } from '@chakra-ui/icons'
+import Footer from '../components/Footer'
+
+
+function Success() {
+
+    const {width, height} = useWindowSize()
+
+   
+
+    return (
+        <>
+            <Box
+                minHeight={{
+                    base: ["-webkit-fill-available", "fill-available", "-moz-available"],
+                    lg: "100vh",
+                }}
+                display="flex"
+                flexDirection="column"
+            >
+                <Flex
+                    minHeight="70vh"
+                    alignItems="center"
+                    justifyContent="center"
+                    mx={10}
+                >
+                    <Stack
+                        spacing={4}
+                        w={"full"}
+                        maxW={"xl"}
+                        bg={useColorModeValue("white", "gray.700")}
+                        rounded={"xl"}
+                        boxShadow={"xl"}
+                        p={10}
+                        my={12}
+                    >
+                        <VStack spacing="30px">
+                            <CheckCircleIcon w={50} h={50} color='green.500' />
+                            <Heading textAlign="center" fontSize="5xl" bgGradient='linear(to-r, green.300, green.600)' backgroundClip="text" >
+                                Successful Payment
+                            </Heading>
+                            <Text fontWeight={"bold"} fontSize={"2xl"} color="green.400">Thank you for your donation!</Text>
+                            <Button
+                                bgGradient='linear(to-r, green.300, green.600)'
+                                size="lg"
+                                color="white"
+
+                                width="200px"
+                                onClick={(e) => handleClick(e)}
+                            >
+                                Back to Home
+                            </Button>
+                        </VStack>
+                    </Stack>
+                </Flex>
+                <Footer />
+
+            </Box>
+            <Confetti
+                height={height} width={width}
+                tweenDuration={30}
+            />
+        </>
+    )
+}
+
+
+
+export default Success
+
